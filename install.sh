7f7ecb58644ddd3d4ff4d4cef80546af3acc9c92
root <root@smiddleton.allgamer.net>
2011-06-08 16:52:41 -0400

    awesome working install script

--- install.sh
+++ install.sh
@@ -48,21 +48,23 @@ function zsh(){
     if [ -f $HOME/.zshrc ] || [ -d $HOME/.oh-my-zsh ]; then
         if $FORCE; then
             echo "Clearing zsh environment"
-            rm -f $HOME/.zshrc $HOME/.oh-my-zsh
+            rm -rf $HOME/.zshrc $HOME/.oh-my-zsh
         else
             echo "You still have a zsh environment, please clean up or use -f"
             exit 1
         fi
     fi
+    echo "Linking .zshrc"
     ln -s $PWD/.zshrc $HOME/.zshrc
-    git clone git@github.com:Rixius/oh-my-zsh.git $HOME/.oh-my-zsh
+    echo "Cloning Personal oh-my-zsh repo"
+    git clone git@github.com:Rixius/oh-my-zsh.git $HOME/.oh-my-zsh >/dev/null 2>&1
 }
 function gitCheck(){
     appCheck git
     if [ ! -f $HOME/.ssh/id_rsa ]; then
         "You don't have a key set up. Fix that and associate it Github"
         exit 1
-    elif ssh git@github.com 2>&1 | grep 'Rixius' >/dev/null 2>&1; then
+    elif ! ssh git@github.com 2>&1 | grep 'Rixius' >/dev/null 2>&1; then
         echo "Your key must be authenticated to your Github account."
         exit 1
     fi
@@ -91,16 +93,19 @@ while getopts "hfavtz" OPTION; do
             exit
             ;;
         v)
+            MULTI=true
             vim
             ;;
         t)
+            MULTI=true
             tmux
             ;;
         z)
+            MULTI=true
             zsh
             ;;
     esac
 done
-
+$MULTI && exit
 help
 exit 1
