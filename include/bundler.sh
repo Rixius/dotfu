08c238dfb9dfad8f00001ec0bdd0f1c653afc332
Stephen "Rixius" Middleton <Stephen@wieck.com>
2011-06-29 10:39:12 -0500

    aded rack defaults

--- include/bundler.sh
+++ include/bundler.sh
@@ -1,13 +1,14 @@
 alias  b="bundle"
 alias bi="b install --path vendor"
-alias bii="b install --path vendor"
+alias bp="b package"
 
-alias bu="b update"
 alias be="b exec"
 
 alias br="be rackup"
-alias sin="br -o sinatra -p 4567 -E development"
 
 alias bu="be unicorn"
 
-alias binit="bi && b package && echo 'vendor/ruby' >> .gitignore"
+alias binit="bi && bp && echo 'vendor/ruby' >> .gitignore"
+
+alias sin="br -o sinatra -p 4567"
+export RACK_ENV='development'
