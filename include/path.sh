e48f0cc8d283fd2677b18043267db88eff70a05f
Stephen Middleton <rixius@gmail.com>
2011-11-16 11:38:50 -0600

    Updates to the folders I need in my path.

--- include/path.sh
+++ include/path.sh
@@ -1,18 +1,13 @@
 export PATH\
 =$HOME/src/dot-fu/bin\
 :$HOME/.bin\
-:$HOME/src/gubna/src\
-:$HOME/src/wieck_scripts\
-:$HOME/src/willGit/bin\
 :/usr/local/bin\
 :/usr/local/sbin\
 :/usr/bin\
 :/bin\
 :/usr/sbin\
 :/sbin\
-:/usr/X11/bin\
-:$HOME/src/android-sdk-mac/tools\
-:$HOME/src/android-sdk-mac/platform-tools
+:/usr/X11/bin
 
 export NODE_PATH=\
 /usr/local/lib/node
