84ea931fad796752f3f87c0d8c1607677f2b9857
Stephen "Rixius" Middleton <Stephen@rixi.us>
2012-12-21 18:25:25 -0600

    prompt updates, including changing all the color variables to a single associative array

--- include/prompt.sh
+++ include/prompt.sh
@@ -4,13 +4,79 @@
 autoload -U colors && colors
 autoload -Uz vcs_info
 
-zstyle ':vcs_info:*' enable git
-zstyle ':vcs_info:git*:*' get-revision true
-zstyle ':vcs_info:git*:*' check-for-changes true
-zstyle ':vcs_info:git*' formats "(%s) %12.12i %c%u %b%m"
-zstyle ':vcs_info:git*' actionformats "(%s|%a) %12.12i %c%u %b%m"
+# Local Variables for Prompt
+local -A c
+
+c+=( 'reset' "%{${reset_color}%}" )
+c+=( 'white' "%{$fg[white]%}" )
+c+=( 'gray' "%{$fg_bold[black]%}" )
+c+=( 'green' "%{$fg_bold[green]%}" )
+c+=( 'red' "%{$fg[red]%}" )
+c+=( 'yellow' "%{$fg[yellow]%}" )
+
+
+function setprompt() {
+	# Establish all variables that are used
+	local -a infoline promptline lines
+
+	# Setup the infoline
+	infoline+=( "${c[red]}⎡${c[reset]}" )
+	[[ $EUID -eq 0 ]] && infoline+=( "${c[red]}" ) || infoline+=( "${c[green]}" )
+	infoline+=( "%n" )
+	infoline+=( "${c[reset]}" )
+	[[ -n $SSH_CLIENT ]] && infoline+=( "@%m" )
+	infoline+=( ":%~" )
+
+	# Setup the promptline
+	promptline+=( "${c[red]}⎣${c[reset]}" )
+	promptline+=( "(" )
+	if [[ $1 -eq 0 ]]; then
+		promptline+=( "${c[green]}" )
+		promptline+=( "✓" )
+	else
+		promptline+=( "${c[red]}" )
+		promptline+=( "✗" )
+	fi
+	promptline+=( "${c[reset]}" )
+	promptline+=( ")" )
+	if `git branch >/dev/null 2>&1`; then
+		promptline+=( "±" )
+	else
+		promptline+=( "≥" )
+	fi
+	promptline+=( " " )
+
+	# Setup the Lines
+	lines+=( ${(j::)infoline} )
+	[[ -n ${vcs_info_msg_0_} ]] && lines+=( "${c[red]}⎢${c[reset]}${c[gray]}${vcs_info_msg_0_}${c[reset]}" )
+	lines+=( ${(j::)promptline} )
+
+	# Finally set the prompt >.>
+	PROMPT=${(F)lines}
+}
+
+function prompt_char {
+    git branch >/dev/null 2>/dev/null && echo "±" && return
+    echo "≥"
+}
+precmd() {
+	# Some manipulation to get the accurate exit status of the last program
+	local lastcall=$?
+	vcs_info
+	setprompt $lastcall
+}
+
+# Overall VCS_Info Settings
+zstyle ':vcs_info:*' enable git svn hg
+zstyle ':vcs_info:(git*|svn*|hg*):*' get-revision true
+zstyle ':vcs_info:(git*|svn*|hg*):*' check-for-changes true
+zstyle ':vcs_info:(git*|svn*|hg*):*' stagedstr "${c[green]}S${c[gray]}"
+zstyle ':vcs_info:(git*|svn*|hg*):*' unstagedstr "${c[red]}U${c[gray]}"
+zstyle ':vcs_info:(git*|svn*|hg*)' formats "(%s) %12.12i %c%u %b%m"
+zstyle ':vcs_info:(git*|svn*|hg*)' actionformats "(%s|%a) %12.12i %c%u %b%m"
 
 zstyle ':vcs_info:git*+set-message:*' hooks git-st git-stash
+
 # Show remote ref and number of commits ahead-of or behind
 function +vi-git-st() {
 	local ahead behind remote
@@ -42,61 +108,3 @@ function +vi-git-stash() {
 		hook_com[misc]+=" (${stashes} stashed)"
 	fi
 }
-
-function setprompt() {
-	# Establish all variables that are used
-	local reset white gray green red
-	local -a infoline promptline lines
-
-	reset="%{${reset_color}%}"
-	white="%{$fg[white]%}"
-	gray="%{$fg_bold[black]%}"
-	green="%{$fg_bold[green]%}"
-	red="%{$fg[red]%}"
-	yellow="%{$fg[yellow]%}"
-
-	# Setup the infoline
-	[[ $EUID -eq 0 ]] && infoline+=( "${red}" ) || infoline+=( "${green}" )
-	infoline+=( "%n" )
-	infoline+=( "${reset}" )
-	[[ -n $SSH_CLIENT ]] && infoline+=( "@%m" )
-	infoline+=( ":%~" )
-
-	# Setup the promptline
-	promptline+=( "(" )
-	if [[ $1 -eq 0 ]]; then
-		promptline+=( "${green}" )
-		promptline+=( "✓" )
-	else
-		promptline+=( "${red}" )
-		promptline+=( "✗" )
-	fi
-	promptline+=( "${reset}" )
-	promptline+=( ")" )
-	if `git branch >/dev/null 2>&1`; then
-		promptline+=( "±" )
-	else
-		promptline+=( "≥" )
-	fi
-	promptline+=( " " )
-
-	# Setup the Lines
-	lines+=( ${(j::)infoline} )
-	[[ -n ${vcs_info_msg_0_} ]] && lines+=( "${gray}${vcs_info_msg_0_}${reset}" )
-	lines+=( ${(j::)promptline} )
-
-	# Finally set the prompt >.>
-	PROMPT=${(F)lines}
-}
-
-function prompt_char {
-    git branch >/dev/null 2>/dev/null && echo "±" && return
-    echo "≥"
-}
-precmd() {
-	# Some manipulation to get the accurate exit status of the last program
-	local lastcall=$?
-	vcs_info
-	setprompt $lastcall
-}
-
