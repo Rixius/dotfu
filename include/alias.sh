696c4342503b67c24cb4916c0c2948dc26478b16
Stephen "Rixius" Middleton <stephen+git@rixi.us>
2013-02-25 10:30:21 -0600

    Fix for last commit.
    I messed up and didn't fully fix a merge conflict :T

--- include/alias.sh
+++ include/alias.sh
@@ -61,19 +61,6 @@ EDITOR='/usr/bin/env vim'
 VISUAL=$EDITOR
 FCEDIT=$EDITOR
 export EDITOR VISUAL FCEDIT
-=======
-alias zz='clear'
-#alias mate='vim -'
-
-alias ls='ls -a --color=tty'
-
-alias google='lynx http://google.com'
-
-alias rload="source $HOME/.zshrc"
-alias :r='source $HOME/.zshrc'
-
-alias  :q='exit'
-alias :wq=':q'
 
 function skey(){
     if [[ "$1" == "true" ]]; then
